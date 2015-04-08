f96f2c037c47085c6fc588916f0786fa2add9e19
Jordan Blissett <jblissett7@gmail.com>
2015-04-08 03:25:22 -0500

    working difficulty window
    just needs aesthetic tweaks to make it look good

--- difficulty.cpp
+++ difficulty.cpp
@@ -3,31 +3,11 @@
 #include "std_lib_facilities_4.h"
 #include "Simple_window.h"
 #include "Graph.h"
+#include "setup_window.h"
 
 int main()
 {
 	using namespace Graph_lib;
-    
-	
-	Point tl{100,0};
-	Simple_window win{tl,800,675,"Difficulty"};
-	
-	Text t{Point{310,50},"FlipFlaps"};
-	t.set_font_size(50);
-	t.set_color(Color::red);
-    
-    enum difficulty_num {two=2,three,four,five,six,seven,eight,nine};
-    Menu difficulty(Point(310,150),50,50,Menu::vertical,"Difficulty");
-    //difficulty.attach(new Button(Point))
-	
-	
-
-
-
-
-
-
-	win.attach(t);
-    win.attach(difficulty);
+    Setup_window win(Point(100,0),500,500,"Difficulty");
 	win.wait_for_button();
 }
