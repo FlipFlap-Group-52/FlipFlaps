d1f4b5b2f1231a4fba034f71de1185b88935d500
Jordan Blissett <jblissett7@gmail.com>
2015-04-08 02:16:37 -0500

    Setup_window class
    used to handle the window and menu call backs for the difficulty

---
+++ setup_window.cpp
@@ -0,0 +1,57 @@
+//
+//  setup_window.cpp
+//  
+//
+//  Created by Jordan Blissett on 4/8/15.
+//
+//
+
+#include "setup_window.h"
+void Setup_window::cb_diff2(Address,Address pw){
+    reference_to<Setup_window>(pw).diff2();
+}
+void Setup_window::diff2(){
+    
+}
+void Setup_window::cb_diff3(Address,Address pw){
+    reference_to<Setup_window>(pw).diff3();
+}
+void Setup_window::diff3(){
+    
+}
+void Setup_window::cb_diff4(Address,Address pw){
+    reference_to<Setup_window>(pw).diff4();
+}
+void Setup_window::diff4(){
+    
+}
+void Setup_window::cb_diff5(Address,Address pw){
+    reference_to<Setup_window>(pw).diff5();
+}
+void Setup_window::diff5(){
+    
+}
+void Setup_window::cb_diff6(Address,Address pw){
+    reference_to<Setup_window>(pw).diff6();
+}
+void Setup_window::diff6(){
+    
+}
+void Setup_window::cb_diff7(Address,Address pw){
+    reference_to<Setup_window>(pw).diff7();
+}
+void Setup_window::diff7(){
+    
+}
+void Setup_window::cb_diff8(Address,Address pw){
+    reference_to<Setup_window>(pw).diff8();
+}
+void Setup_window::diff8(){
+    
+}
+void Setup_window::cb_diff9(Address,Address pw){
+    reference_to<Setup_window>(pw).diff9();
+}
+void Setup_window::diff9(){
+    
+}
---
+++ setup_window.h
@@ -0,0 +1,39 @@
+//
+//  setup_window.h
+//  this will be used for the difficulty buttons and window setup
+//
+//
+//
+//
+
+#ifndef ____setup_window__
+#define ____setup_window__
+
+#include <stdio.h>
+#include "Simple_window.h"
+#include "Graph.h"
+using namespace Graph_lib;
+class Setup_window :public Simple_window{
+public:
+    Setup_window(Point xy, int w,int h, const string& s): Simple_window(xy,w,h,s){}
+   
+private:
+    static void cb_diff2(Address,Address);
+    void diff2();
+    static void cb_diff3(Address,Address);
+    void diff3();
+    static void cb_diff4(Address,Address);
+    void diff4();
+    static void cb_diff5(Address,Address);
+    void diff5();
+    static void cb_diff6(Address,Address);
+    void diff6();
+    static void cb_diff7(Address,Address);
+    void diff7();
+    static void cb_diff8(Address,Address);
+    void diff8();
+    static void cb_diff9(Address,Address);
+    void diff9();
+    
+};
+#endif /* defined(____setup_window__) */
