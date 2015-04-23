dd1d5b86f81d949cf94bb2012cbe12ce8cf6fe08
Jordan Blissett <jblissett7@gmail.com>
2015-04-23 16:42:30 -0500

    high_score_window

---
+++ high_score_window.cpp
@@ -0,0 +1,9 @@
+//
+//  high_score_window.cpp
+//  
+//
+//  Created by Jordan Blissett on 4/23/15.
+//
+//
+
+#include "high_score_window.h"
---
+++ high_score_window.h
@@ -0,0 +1,14 @@
+//
+//  high_score_window.h
+//  
+//
+//  Created by Jordan Blissett on 4/23/15.
+//
+//
+
+#ifndef ____high_score_window__
+#define ____high_score_window__
+
+#include <stdio.h>
+
+#endif /* defined(____high_score_window__) */
