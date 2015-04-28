82e02e49ee2d1e2041c59dc0d9f86f1a945467e3
Jordan Blissett <jblissett7@gmail.com>
2015-04-28 16:19:28 -0500

    added file

--- high_score_window.cpp
+++ high_score_window.cpp
@@ -21,3 +21,16 @@ the_winners(Point(250,150),"Top 5 Scores")
     attach(title);
     attach(the_winners);
 }
+void high_score_window::write_score(string s,string t){
+    ofs.open("Score_file.txt");
+    if (ofs.is_open()) {
+        ofs<< s << "   " << t;
+        ofs.close();
+    }
+}
+void high_score_window::read_score(){
+    ifs.open("Score_file.txt");
+    if (ifs.is_open()) {
+        <#statements#>
+    }
+}
--- high_score_window.h
+++ high_score_window.h
@@ -17,10 +17,14 @@ using namespace Graph_lib;
 class high_score_window: public Graph_lib::Window{
 public:
 	high_score_window(Point xy, int w,int h, const string& s);
-	
+    
+    void write_score(string s,string t){}
+    void read_score(){}
 private:
 	Text title;
 	Text the_winners;
+    ofstream ofs;
+    ifstream ifs;
 	
 
 };
